1ae72f89e9e1a6cbc6e9649639c39ba7dface3fc
srgvrbv <i@svorobyov.ru>
2024-06-02 20:02:26 +0300

    Add files via upload

---
+++ houmework2.py/main.py
@@ -0,0 +1,6 @@
+
+amount_of_homework_done = 12
+number_of_hours = 1.5
+name = "Курс: Python"
+time_for_one_task = number_of_hours / amount_of_homework_done
+print(name,',','всего задач:',amount_of_homework_done,',затрачено часов:',number_of_hours,',среднее время выполнения:',time_for_one_task,'часа')
